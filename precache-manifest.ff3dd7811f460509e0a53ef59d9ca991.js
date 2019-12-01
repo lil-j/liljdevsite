6057e7954bbcd97f8961c5e4a8d4d94bfd0aa36f
jake <jke.hrpr@gmail.com>
2019-12-01 15:24:49 -0800

    Updates

---
+++ precache-manifest.ff3dd7811f460509e0a53ef59d9ca991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b467bda12ab308500e09a6807c642914",
+    "url": "/liljdevsite/index.html"
+  },
+  {
+    "revision": "c3d58532ec1f9b8a3883",
+    "url": "/liljdevsite/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f5f48c1614ef7a2eead3",
+    "url": "/liljdevsite/static/css/main.79ecab86.chunk.css"
+  },
+  {
+    "revision": "c3d58532ec1f9b8a3883",
+    "url": "/liljdevsite/static/js/2.31b5dafb.chunk.js"
+  },
+  {
+    "revision": "f5f48c1614ef7a2eead3",
+    "url": "/liljdevsite/static/js/main.edf03848.chunk.js"
+  },
+  {
+    "revision": "fd1b75ae931179dc7fa3",
+    "url": "/liljdevsite/static/js/runtime-main.a16ba480.js"
+  },
+  {
+    "revision": "5eeaab6850aab5765ef94b0b34904267",
+    "url": "/liljdevsite/static/media/Tomatoes.5eeaab68.ttf"
+  }
+]);
